757ff7cae58b9f5af61c706f9a3dceae447e5d39
Arseniy <arseniy@neburalis.space>
2025-06-20 17:56:32 +0300

    Update vox_example.py

--- vox_example.py
+++ vox_example.py
@@ -2,6 +2,7 @@ from vox.api import VoxAPI
 from vox.models import Subject
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
@@ -11,11 +12,18 @@ nickname = 'narhipovd'
 user_id = vox.get_user_id(nickname)['id']
 
 ai_analytic = vox.ai_analytics(subject=Subject.USER, subject_id=user_id)
+if 'report' in ai_analytic:
+    ai_analytic = ai_analytic['report']
 print(ai_analytic)
 
 report = vox.custom_report(subject=Subject.USER, subject_id=user_id,
                            custom_prompt=str(ai_analytic)+"\n\nНапиши предсказание для этого человека на неделю. " +\
                            "Пиши в стиле гадания на картах таро. Укажи название карт и что это значит. " +\
-                           "Используй эмодзи в меру.")['report']
+                           "В начале напиши что раскладываешь карты. Отделяй каждую карту и ее описание \\n\\n. " +\
+                           "Используй эмодзи. Не ссылайся на активность в конкретных каналах и чатах.")
+if 'report' in report:
+    report = json.loads(report['report'])
+    if 'report' in report:
+        report = report['report']
 print(report)
 
